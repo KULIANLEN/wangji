be028535b55c7db677a022f4fef2e9e35ffd463a
hanzif1 <106012635+hanzif1@users.noreply.github.com>
2023-10-07 13:23:35 +0800

    引用style中的index.css

--- luotuozheng/main.js
+++ luotuozheng/main.js
@@ -3,6 +3,7 @@ import App from './App'
 // #ifndef VUE3
 import Vue from 'vue'
 import './uni.promisify.adaptor'
+import './style/index.css'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
