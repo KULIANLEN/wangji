409fa1e1043beb19629c90935c3f15162a0d3741
hanzif1 <106012635+hanzif1@users.noreply.github.com>
2023-10-07 14:54:00 +0800

    wq

--- luotuozheng/main.js
+++ luotuozheng/main.js
@@ -3,7 +3,6 @@ import App from './App'
 // #ifndef VUE3
 import Vue from 'vue'
 import './uni.promisify.adaptor'
-import './style/index.css'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
